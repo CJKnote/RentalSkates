4284b5826f077868802e19fe18a62d8abdb3f65a
Caroline Knote <cj.knote@gmail.com>
2026-05-14 17:31:04 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,21 @@
 
 const $skateForm = $('#newSkateForm');
 const $skateStorage = $('#skateStorage');
+var Excel = require("exceljs");
+var workbook = new Excel.Workbook();
+workbook.xlsx.readFile('TestData.xlsx')
+.then(function() {
+    var worksheet - workbook.getWorksheet('sheet');
+    var i=1;
+    worksheet.eachRow({includeEmpty: false}, function(row, rowNumber) {
+        r-worksheet.getRow(i).values;
+        r1=r[2]; //index the column
+        console.log(r1);
+        i++;
+    });
+    worksheet.getCell('B3').value = "test";
+    return workbook.xlsx.writeFile('TestData.xlsx');
+});
 
 $charForm.on("submit", function(e){
     e.preventDefault();
@@ -19,6 +34,10 @@ $charForm.on("submit", function(e){
 
     displayChar(skateName);
 });
+
+function changeLog(skateForm){
+    
+}
 
 function displaySkate(skateName){
     let skate = $("<p></p>").text(skateName);
